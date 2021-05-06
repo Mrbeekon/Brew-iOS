270ecf274ae61151d378a49bdc2d5109c676a43f
Sam <kirkysam9@gmail.com>
2021-05-06 15:25:02 +0100

    Deletes correct item

--- Bubbler/Bubbler/Models/BrewEntity+CoreDataClass.swift
+++ Bubbler/Bubbler/Models/BrewEntity+CoreDataClass.swift
@@ -71,23 +71,6 @@ public class BrewEntity: NSManagedObject {
     
     //adds a sanitised reading to the brew
     func addReading(readingDate: Date, readingValue: String){
-        /*var cleanReadingValue = readingValue
-         if cleanReadingValue.contains(".") {
-             let splitted = cleanReadingValue.split(separator: ".")
-             if splitted.count >= 2 {
-                 let preDecimal = String(splitted[0])
-                 var afterDecimal = String(splitted[1])
-                 
-                 var afterDecimalArray = afterDecimal.components(separatedBy: "")
-                 
-                 let c = 1...(2-afterDecimalArray.count)
-                 for _ in c {
-                     afterDecimalArray.append("0")
-                 }
-                 afterDecimal = afterDecimalArray.joined()
-                 cleanReadingValue = "\(preDecimal).\(afterDecimal)"
-             }
-         }*/
         //only one decimal allowed in string
         var cleanReadingValue = readingValue
         if cleanReadingValue.contains(".") {
@@ -101,5 +84,15 @@ public class BrewEntity: NSManagedObject {
         //should be 3dp as is standard
         cleanReadingValue = String(format: "%.3f", ((cleanReadingValue) as NSString).doubleValue)
         self.readings[readingDate] = cleanReadingValue
+        
+        self.sortReadings()
+    }
+    
+    func sortReadings() {
+        var newDict:[Date:String] = [:]
+        for tuple in self.readings.sorted(by: <) {
+            newDict[tuple.key] = tuple.value
+        }
+        self.readings = newDict
     }
 }
